9b3c853720d155911e5428b21945cd91a807709b
Etsushi <etsushi.otani@emoshu.co.jp>
2022-01-16 01:06:32 +0900

    [add] GithubRepositoryRepository追加

---
+++ iOSEngineerCodeCheck/Repositories/GithubRepositoryRepository.swift
@@ -0,0 +1,27 @@
+//
+//  GithubRepositoryRepository.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Etsushi Otani on 2022/01/16.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import Combine
+import Foundation
+
+protocol GithubRepositoryRepositoryProtocol {
+    func searchRepositories(by word: String) -> AnyPublisher<SearchRepositoriesResponse, Error>
+}
+
+final class GithubRepositoryRepository: GithubRepositoryRepositoryProtocol {
+    private(set) var response: SearchRepositoriesResponse?
+
+    func searchRepositories(by word: String) -> AnyPublisher<SearchRepositoriesResponse, Error> {
+        SearchRepositoriesRequest(searchWord: word).exec()
+            .tryMap { [weak self] response -> SearchRepositoriesResponse in
+                self?.response = response
+                return response
+            }
+            .eraseToAnyPublisher()
+    }
+}
