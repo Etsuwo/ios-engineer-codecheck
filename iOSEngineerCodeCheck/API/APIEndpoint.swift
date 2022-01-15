6434d757f29a601784cf58414bb57ae9fcd1b4e0
Etsushi <etsushi.otani@emoshu.co.jp>
2022-01-15 22:01:23 +0900

    [add] APIのエンドポイントを定義するファイル追加

---
+++ iOSEngineerCodeCheck/API/APIEndpoint.swift
@@ -0,0 +1,13 @@
+//
+//  GithubAPI.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Etsushi Otani on 2022/01/15.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
+
+struct APIEndpoint {
+    static let baseURL = "https://api.github.com"
+}
