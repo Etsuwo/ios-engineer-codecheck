a560456580ab7d7bb0f085bda04f67a765e522b6
Etsushi <etsushi.otani@emoshu.co.jp>
2022-01-18 01:22:29 +0900

    [update] cellの設定用メソッド追加

--- iOSEngineerCodeCheck/Screen/Search/RepositorySearchCell.swift
+++ iOSEngineerCodeCheck/Screen/Search/RepositorySearchCell.swift
@@ -7,16 +7,23 @@
 //
 
 import Foundation
+import Kingfisher
 import UIKit
 
 final class RepositorySearchCell: UITableViewCell {
-    
     @IBOutlet private weak var avatarImageView: UIImageView!
     @IBOutlet private weak var ownerNameLabel: UILabel!
     @IBOutlet private weak var repositoryNameLabel: UILabel!
     @IBOutlet private weak var descriptionLabel: UILabel!
     @IBOutlet private weak var detailLabel: UILabel!
-    
+
     static let identifier = "RepositorySearchCell"
-    
+
+    func configure(with item: Item) {
+        avatarImageView.kf.setImage(with: URL(string: item.owner.avatarUrl))
+        ownerNameLabel.text = L10n.RepositorySearch.OwnerNameLabel.text(item.owner.login)
+        repositoryNameLabel.text = item.name
+        descriptionLabel.text = item.description
+        detailLabel.text = L10n.RepositorySearch.DetailLabel.text(item.language ?? L10n.Common.blank, item.stargazersCount)
+    }
 }
