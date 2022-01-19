6ed0491b897f408d1cf4d0c4bc23f3c1ebaac46f
Etsushi <etsushi.otani@emoshu.co.jp>
2022-01-19 19:24:34 +0900

    [add] IndicatorViewModel作成

---
+++ iOSEngineerCodeCheck/Screen/Search/IndicatorViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  IndicatorViewModel.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Etsushi Otani on 2022/01/19.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import Combine
+import Foundation
+
+protocol IndicatorViewModelOutputs {
+    var isLoading: AnyPublisher<Bool, Never> { get }
+}
+
+protocol IndicatorViewModelType {
+    var outputs: IndicatorViewModelOutputs { get }
+}
+
+final class IndicatorViewModel: IndicatorViewModelType {
+    var outputs: IndicatorViewModelOutputs { self }
+    private let repository: SearchRepositoryRepositoryProtocol
+
+    init(repository: SearchRepositoryRepositoryProtocol) {
+        self.repository = repository
+    }
+}
+
+extension IndicatorViewModel: IndicatorViewModelOutputs {
+    var isLoading: AnyPublisher<Bool, Never> {
+        repository.isLoading
+    }
+}
