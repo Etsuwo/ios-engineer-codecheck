1a5d811546a3f623e65011ac59dee026c6729182
Etsushi <etsushi.otani@emoshu.co.jp>
2022-01-20 10:30:20 +0900

    [update] Readmeを保持する責務をViewModelからRepositoryに移動

--- iOSEngineerCodeCheck/Repositories/GetReadmeRepository.swift
+++ iOSEngineerCodeCheck/Repositories/GetReadmeRepository.swift
@@ -10,11 +10,17 @@ import Combine
 import Foundation
 
 protocol GetReadmeRepositoryProtocol {
-    func getReadme(owner: String, repository: String) -> AnyPublisher<GetReadmeResponse, Error>
+    func getReadme(owner: String, repository: String)
+    var readme: AnyPublisher<String, Never> { get }
 }
 
 final class GetReadmeRepository: GetReadmeRepositoryProtocol {
     private let provider: GithubAPIProviderProtocol
+    private let readmeSubject = PassthroughSubject<String, Never>()
+    private var cancellable: AnyCancellable?
+    var readme: AnyPublisher<String, Never> {
+        readmeSubject.eraseToAnyPublisher()
+    }
 
     init(provider: GithubAPIProviderProtocol = GithubAPIProvider()) {
         self.provider = provider
@@ -24,10 +30,20 @@ final class GetReadmeRepository: GetReadmeRepositoryProtocol {
     /// - Parameters:
     ///   - owner: リポジトリ所有者の名前
     ///   - repository: リポジトリ名
-    /// - Returns: GetReadmeResponseを流すPublisher
-    func getReadme(owner: String, repository: String) -> AnyPublisher<GetReadmeResponse, Error> {
+    func getReadme(owner: String, repository: String) {
         let request = GetReadmeRequest(owner: owner, repo: repository)
-        return provider.exec(with: request)
-            .eraseToAnyPublisher()
+        cancellable = provider.exec(with: request)
+            .sink(receiveCompletion: { completion in
+                if case let .failure(error) = completion {
+                    print(error.localizedDescription)
+                }
+            }, receiveValue: { [weak self] data in
+                guard let data = Data(base64Encoded: data.content, options: .ignoreUnknownCharacters),
+                      let readme = String(data: data, encoding: .utf8)
+                else {
+                    return
+                }
+                self?.readmeSubject.send(readme)
+            })
     }
 }
